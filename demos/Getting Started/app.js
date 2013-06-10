f00696af6f9088a8665985572350635e3dfc9082
Raymond Camden <raymondcamden@gmail.com>
2013-06-10 07:09:31 -0500

    initial seed

---
+++ demos/Getting Started/app.js
@@ -0,0 +1,12 @@
+function cowbell(x) {
+    'use strict';
+    return "You rang the cowbell " + x + " times.";
+}
+
+function total(a, b) {
+    'use strict';
+    return a+b;
+}
+
+x = cowbell(9);
+z = total(1,3);
